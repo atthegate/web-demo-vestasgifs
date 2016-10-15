6591e4259bd8cb26c62572c6aea15cb1d8aa0de6
atthegate <valhalla@Jons-MacBook-Pro.local>
2016-10-15 13:09:04 -0700

    first commit

---
+++ vgifs.js
@@ -0,0 +1,386 @@
+playlist = [
+	{
+	'gfyName': 'AnotherImprobableEider',
+	'type':'giant',
+	'duration':16.8,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'TotalCautiousCockatiel',
+	'type':'zippy',
+	'duration':1.4,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'HopefulSimpleChamois',
+	'type':'zippy',
+	'duration':1.4,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'FakeAnguishedBufflehead',
+	'type':'zippy',
+	'duration':0.6,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'WealthyPiercingHeifer',
+	'type':'zippy',
+	'duration':1.7,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'SentimentalEcstaticFunnelweaverspider',
+	'type':'zippy',
+	'duration':0.3,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'ConfusedAcclaimedBadger',
+	'type':'zippy',
+	'duration':1.6,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'BitterOrnateArgentineruddyduck',
+	'type':'zippy',
+	'duration':1.9,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'EmotionalBiodegradableIraniangroundjay',
+	'type':'giant',
+	'duration':4.2,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'ColorfulDistinctHarvestmen',
+	'type':'zippy',
+	'duration':1.7,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'WelcomeSoreAdmiralbutterfly',
+	'type':'fat',
+	'duration':10.1,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'DemandingDefinitiveAnkole',
+	'type':'zippy',
+	'duration':4.9,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'BogusZanyBasil',
+	'type':'giant',
+	'duration':14.7,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'FalseResponsibleJaeger',
+	'type':'giant',
+	'duration':12.6,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'UnimportantZestyGartersnake',
+	'type':'giant',
+	'duration':18.8,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'SecondaryDeliciousBoubou',
+	'type':'giant',
+	'duration':33.8,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'MagnificentWelcomeAnaconda',
+	'type':'zippy',
+	'duration':4.6,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'IckyElatedIcelandicsheepdog',
+	'type':'fat',
+	'duration':7.4,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'NarrowJealousIndigobunting',
+	'type':'fat',
+	'duration':11.5,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'PointedImpartialAegeancat',
+	'type':'giant',
+	'duration':19.2,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'SereneDearestKawala',
+	'type':'giant',
+	'duration':17.4,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'BlondSecondhandFrigatebird',
+	'type':'giant',
+	'duration':13.0,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'KeenColdAndeancat',
+	'type':'giant',
+	'duration':11.3,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'MadOldfashionedHeterodontosaurus',
+	'type':'zippy',
+	'duration':2.2,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'EcstaticComplicatedBluet',
+	'type':'giant',
+	'duration':13.3,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'FirmPortlyBeagle',
+	'type':'fat',
+	'duration':2.3,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'InfantilePossibleDuiker',
+	'type':'fat',
+	'duration':5.8,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'ActualSillyGaur',
+	'type':'zippy',
+	'duration':1.6,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'AbsoluteWellmadeAsiaticwildass',
+	'type':'fat',
+	'duration':5.8,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'ImpossibleNiceGypsymoth',
+	'type':'giant',
+	'duration':10.4,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'ImpressionableReflectingFirebelliedtoad',
+	'type':'fat',
+	'duration':4.2,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'EasygoingIncomparableArabianwildcat',
+	'type':'giant',
+	'duration':21.2,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'HideousThankfulGrison',
+	'type':'giant',
+	'duration':16.7,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'RingedBreakableHedgehog',
+	'type':'fat',
+	'duration':13.2,
+	'description': '',
+	'reverse': false,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'NarrowFamousCricket',
+	'type':'fat',
+	'duration':8.2,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'GraveMiserlyEmeraldtreeskink',
+	'type':'zippy',
+	'duration':1.8,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'EnviousUnfortunateAcornbarnacle',
+	'type':'zippy',
+	'duration':1.7,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'OrangeCreamyAmazontreeboa',
+	'type':'zippy',
+	'duration':1.7,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'IcyFaithfulKiskadee',
+	'type':'zippy',
+	'duration':3.1,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'ElatedMarvelousGander',
+	'type':'zippy',
+	'duration':3.8,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'SophisticatedGoodAnglerfish',
+	'type':'zippy',
+	'duration':1.7,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'AngryDefensiveCopepod',
+	'type':'zippy',
+	'duration':1.6,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'BackBaggyIrukandjijellyfish',
+	'type':'giant',
+	'duration':11.0,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'CoarseFarawayAcornwoodpecker',
+	'type':'giant',
+	'duration':4.9,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'BraveRespectfulAuk',
+	'type':'zippy',
+	'duration':1.6,
+	'description': '',
+	'reverse': false,
+	'perfectloop': true
+	},
+	{
+	'gfyName': 'PotableElatedGelding',
+	'type':'fat',
+	'duration':9.6,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'PointedEverlastingAnnashummingbird',
+	'type':'zippy',
+	'duration':5.2,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+	{
+	'gfyName': 'UnlawfulElementaryDassierat',
+	'type':'zippy',
+	'duration':5.4,
+	'description': '',
+	'reverse': true,
+	'perfectloop': false
+	},
+]
